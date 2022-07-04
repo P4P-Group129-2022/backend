004065a335a608e6a797a41fceecd5dca9fc9b9e
jshi911 <wndyddld12@gmail.com>
2022-07-04 14:35:29 +1200

    Make DTOs.

---
+++ src/DTOs/ApiTypes.ts
@@ -0,0 +1,8 @@
+/**
+ * This file contains the types used by the controllers when sending and receiving data.
+ */
+export type Colleague = {
+    nameId: string,
+    name: string;
+    profileImgUrl: string;
+}
--- src/models/Message.ts
+++ src/models/Message.ts
@@ -1,5 +1,5 @@
 import mongoose, {Document, Schema} from 'mongoose';
-import Colleague from "./Colleague";
+import {Colleague} from "../DTOs/ApiTypes";
 
 /**
  * Message model containing the content of the message.
