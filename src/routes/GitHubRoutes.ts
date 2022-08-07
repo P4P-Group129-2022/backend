f2d131bb54315649b573202b37abca94f1a726a6
Dave Shin <100652749+hiin3d55@users.noreply.github.com>
2022-08-07 20:56:41 +1200

    Changed the uri of the endpoint. Also changed the endpoint to check the user's correct repo.

--- src/routes/GitHubRoutes.ts
+++ src/routes/GitHubRoutes.ts
@@ -6,7 +6,7 @@ const router = express();
 router.get("/", (_, res) => {
     res.json({ message: "You are connected to backend github api" });
 });
-router.get("/:pullNumber", GitHubController.checkPR);
+router.get("/:username/:pullNumber", GitHubController.checkPR);
 router.put("/update", GitHubController.updateGitHubDetails);
 router.get("/invite/:username", GitHubController.inviteToOrganization);
 
