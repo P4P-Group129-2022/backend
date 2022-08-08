67e0c865607f6034b14e32a213ef3ec0b47d1a81
Hyung Park <hpar461@aucklanduni.ac.nz>
2022-08-09 11:48:53 +1200

    Update user model.

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,22 +1,24 @@
-import mongoose, {Document, Schema} from 'mongoose';
+import mongoose, { Document, Schema } from "mongoose";
 
 /**
  * Model class for user that will be using this tool.
  */
 export interface IUser {
-    gitHubUsername: string;
-    email: string;
-    displayName: string;
+  githubUsername: string;
+  email: string;
+  displayName: string;
+  completedPreTest: boolean;
 }
 
 export interface IUserModel extends IUser, Document {
 }
 
 const UserSchema: Schema = new Schema(
-    {
-        githubUsername: {type: String, required: true},
-        email: {type: String, required: true},
-        displayName: {type: String, required: true},
-    });
+  {
+    githubUsername: { type: String, required: true },
+    email: { type: String, required: true },
+    displayName: { type: String, required: true },
+    completedPreTest: { type: Boolean, required: true },
+  });
 
-export default mongoose.model<IUserModel>('User', UserSchema);
+export default mongoose.model<IUserModel>("User", UserSchema);
