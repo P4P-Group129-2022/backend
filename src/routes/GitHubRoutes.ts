8b268925446d50420ba9f1e0b7c7fccbcb172ec6
Hyung Park <hpar461@aucklanduni.ac.nz>
2022-08-09 10:21:53 +1200

    Rearrange hierarchy of github endpoints

--- src/routes/GitHubRoutes.ts
+++ src/routes/GitHubRoutes.ts
@@ -6,8 +6,8 @@ const router = express();
 router.get("/", (_, res) => {
     res.json({ message: "You are connected to backend github api" });
 });
-router.get("/:username/:pullNumber", GitHubController.checkPR);
-router.put("/update", GitHubController.updateGitHubDetails);
 router.get("/invite/:username", GitHubController.inviteToOrganization);
+router.put("/update", GitHubController.updateGitHubDetails);
+router.get("/checkPR/:username/:pullNumber", GitHubController.checkPR);
 
 export default router;
