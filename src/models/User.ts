32fd16109f5fe7e0f61dcd25eb3cc72ab8b7d216
Dave Shin <100652749+hiin3d55@users.noreply.github.com>
2022-08-10 10:30:19 +1200

    Added a field.

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,24 +1,26 @@
-import mongoose, { Document, Schema } from "mongoose";
+import mongoose, {Document, Schema} from "mongoose";
 
 /**
  * Model class for user that will be using this tool.
  */
 export interface IUser {
-  githubUsername: string;
-  email: string;
-  displayName: string;
-  completedPreTest: boolean;
+    githubUsername: string;
+    email: string;
+    displayName: string;
+    completedPreTest: boolean;
+    currentScenario: number;
 }
 
 export interface IUserModel extends IUser, Document {
 }
 
 const UserSchema: Schema = new Schema(
-  {
-    githubUsername: { type: String, required: true },
-    email: { type: String, required: true },
-    displayName: { type: String, required: true },
-    completedPreTest: { type: Boolean, required: true },
-  });
+    {
+        githubUsername: {type: String, required: true},
+        email: {type: String, required: true},
+        displayName: {type: String, required: true},
+        completedPreTest: {type: Boolean, required: true},
+        currentScenario: {type: Number, required: true},
+    });
 
 export default mongoose.model<IUserModel>("User", UserSchema);
