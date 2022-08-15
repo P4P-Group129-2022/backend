3e9a13ba8679c80b8422399b56350f051a0874b3
Hyung Park <hpar461@aucklanduni.ac.nz>
2022-08-15 14:31:31 +1200

    Remove main.py and change it to index.html

--- repos/scenarioDefaults/scenario-2/main.py
+++
@@ -1,9 +0,0 @@
-def very_important_function():
-    print("This is a very important function")
-    return True
-BUG!!!!REMOVE_ME
-def main():
-    very_important_function()
-
-if __name__ == "__main__":
-    main()
